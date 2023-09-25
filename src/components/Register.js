8a35e78f2e63ee4a48ab8a527b1878df1da9a631
Metalli01 <kbkannan2001@gmail.com>
2023-09-25 08:30:37 +0530

    first commit

---
+++ src/components/Register.js
@@ -0,0 +1,163 @@
+import React from "react";
+import { TextField, Button, Stack } from "@mui/material";
+import "./Register.css";
+import { useState } from "react";
+function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cpass, setCPass] = useState("");
+
+  const [isUsernameValid, setisUsernameValid] = useState(true);
+  const [isEmailValid, setisEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+  const [isCpasswordValid, setIsCpasswordValid] = useState(true);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!username || !password || !email || !cpass) {
+      alert("Please fill the form completely!");
+    }
+    else if(password!==cpass){
+      alert("passwords do not match enter correctly !!")
+    }
+     else
+      alert(`
+             Username : ${username}
+             Email : ${email}
+             Password ${password}'
+             confirmation : ${cpass}
+            `);
+  };
+
+  const handleReset = () => {
+    setUsername("");
+    setEmail("");
+    setPassword("");
+    setCPass("");
+  };
+
+  const validateInput = (e) => {
+    const { value, name } = e.target;
+    if (!!value.match(/^[a-zA-Z0-9_]+$/)) {
+      setUsername(value);
+      setisUsernameValid(true);
+    } else {
+      setisUsernameValid(false);
+    }
+  };
+
+  const validateEmail = (e) => {
+    const inputEmail = e.target.value;
+    setEmail(inputEmail);
+    const pattern = /^[\w\.-]+@[\w\.-]+\.\w+$/;
+    const isEmailValid = pattern.test(inputEmail);
+    setisEmailValid(isEmailValid);
+  };
+
+  const validatePassword = (e) => {
+    const inputPassword = e.target.value;
+    setPassword(inputPassword);
+    const pattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8,}$/;
+    const isPasswordValid = pattern.test(inputPassword);
+    setIsPasswordValid(isPasswordValid);
+  };
+
+  const validateCpassword = (e) => {
+    const inputPassword = e.target.value;
+    setCPass(inputPassword);
+    if (password === inputPassword) {
+      setIsCpasswordValid(true);
+    } else {
+      setIsCpasswordValid(false);
+    }
+  };
+
+  return (
+    <div className="Home">
+      <div className="card">
+        <h1 style={{ textAlign: "center" }}>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="Text-area">
+            <TextField
+              id="standard-basic"
+              label="Name"
+              variant="standard"
+              className="Text-field"
+              name="username"
+              value={username || ""}
+              onChange={(e) => validateInput(e)}
+            />
+          </div>
+          {!isUsernameValid && (
+            <div className="mb-3 text-danger">*invalid input</div>
+          )}
+
+          <div className="Text-area">
+            <TextField
+              id="standard-basic"
+              label="Email"
+              variant="standard"
+              className="Text-field"
+              name="email"
+              value={email || ""}
+              onChange={(e) => validateEmail(e)}
+            />
+          </div>
+          {!isEmailValid && (
+            <div className="mb-3 text-danger">* Enter the email correctly</div>
+          )}
+
+          <div className="Text-area">
+            <TextField
+              type="password"
+              id="standard-basic"
+              label="Password"
+              variant="standard"
+              className="Text-field"
+              name="password"
+              value={password || ""}
+              onChange={(e) => validatePassword(e)}
+            />
+          </div>
+
+          {!isPasswordValid && (
+            <div className="mb-3 text-danger">*Create a strong password</div>
+          )}
+          <div className="Text-area">
+            <TextField
+              type="password"
+              id="standard-basic"
+              label="Confirm Password"
+              variant="standard"
+              className="Text-field"
+              name="cpass"
+              value={cpass || ""}
+              onChange={(e) => validateCpassword(e)}
+            />
+          </div>
+          {!isCpasswordValid && (
+            <div className="mb-3 text-danger">* Passwords do not match</div>
+          )}
+
+          <div style={{ textAlign: "center" }}>
+            <Button type="submit" className="btnstyle" onClick={handleSubmit}>
+              Sign Up
+            </Button>
+            <Button onClick={handleReset} className="btnstyle">
+              Reset
+            </Button>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            Have an Account ?{" "}
+            <a href="" className="links">
+              Login Here
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
